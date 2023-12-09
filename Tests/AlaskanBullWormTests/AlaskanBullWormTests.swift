0e6f8eec384c3bdcfe8b9707f34d40c2e39c5103
Aaron Rennow <arennow@outlook.com>
2023-12-09 08:24:22 -0500

    Fix repeated typo

--- Tests/AlaskanBullWormTests/AlaskanBullWormTests.swift
+++ Tests/AlaskanBullWormTests/AlaskanBullWormTests.swift
@@ -4,61 +4,61 @@ import XCTest
 final class AlaskanBullWormTests: XCTestCase {
 	func testProcessLine_multiple() throws {
 		let src = "123\n456\n789"
-		var abm = AlaskanBullWorm(source: src)
+		var abw = AlaskanBullWorm(source: src)
 
-		let num = try abm.processLine { line in
+		let num = try abw.processLine { line in
 			XCTAssertEqual(line, "123")
 			return Int(line)
 		}
 		XCTAssertEqual(num, 123)
-		XCTAssertEqual(abm.remainder, "456\n789")
+		XCTAssertEqual(abw.remainder, "456\n789")
 	}
 
 	func testProcessLine_single() throws {
 		let src = "123"
-		var abm = AlaskanBullWorm(source: src)
+		var abw = AlaskanBullWorm(source: src)
 
-		let num = try abm.processLine { line in
+		let num = try abw.processLine { line in
 			XCTAssertEqual(line, "123")
 			return Int(line)
 		}
 		XCTAssertEqual(num, 123)
-		XCTAssertEqual(abm.remainder, "")
+		XCTAssertEqual(abw.remainder, "")
 	}
 
 	func testProcessLine_empty() {
-		var abm = AlaskanBullWorm(source: "")
+		var abw = AlaskanBullWorm(source: "")
 
-		XCTAssertThrowsError(try abm.processLine { _ in }) { error in
+		XCTAssertThrowsError(try abw.processLine { _ in }) { error in
 			XCTAssert(error is AlaskanBullWorm.Errors.NotEnoughRemiainder)
 		}
-		XCTAssertEqual(abm.remainder, "")
+		XCTAssertEqual(abw.remainder, "")
 	}
 
 	func testTakeLinesUntil_middle() throws {
 		let src = "123\n456\n\n789"
-		var abm = AlaskanBullWorm(source: src)
+		var abw = AlaskanBullWorm(source: src)
 
-		let chunk = try abm.takeLines(until: { $0 == "" })
+		let chunk = try abw.takeLines(until: { $0 == "" })
 		XCTAssertEqual(chunk, ["123", "456"])
-		XCTAssertEqual(abm.remainder, "789")
+		XCTAssertEqual(abw.remainder, "789")
 	}
 
 	func testTakeLinesUntil_end() throws {
 		let src = "123\n456\n789"
-		var abm = AlaskanBullWorm(source: src)
+		var abw = AlaskanBullWorm(source: src)
 
-		let chunk = try abm.takeLines(until: { $0 == "" })
+		let chunk = try abw.takeLines(until: { $0 == "" })
 		XCTAssertEqual(chunk, ["123", "456", "789"])
-		XCTAssertEqual(abm.remainder, "")
+		XCTAssertEqual(abw.remainder, "")
 	}
 
 	func testProcessLinesToEnd() throws {
 		let src = "123\n456\n789"
-		var abm = AlaskanBullWorm(source: src)
+		var abw = AlaskanBullWorm(source: src)
 
-		let output = try abm.processLinesToEnd { line in Int(line)! }
+		let output = try abw.processLinesToEnd { line in Int(line)! }
 		XCTAssertEqual(output, [123, 456, 789])
-		XCTAssertEqual(abm.remainder, "")
+		XCTAssertEqual(abw.remainder, "")
 	}
 }
