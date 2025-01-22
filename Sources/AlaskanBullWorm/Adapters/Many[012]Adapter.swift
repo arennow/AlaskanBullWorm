fe5ab93d507a0c2e0e577b706a59ab9e6e3f5276
Aaron Rennow <arennow@outlook.com>
2025-01-22 06:15:46 -0500

    Add `many2`

---
+++ Sources/AlaskanBullWorm/Adapters/Many[012]Adapter.swift
@@ -0,0 +1,19 @@
+public func many0<T>(_ inner: some Parser<T>) -> some Parser<Array<T>> {
+	InlineParser { input in
+		(0...).mapUntilNil { _ in
+			inner.parse(&input)
+		}
+	}
+}
+
+public func many1<T>(_ inner: some Parser<T>) -> some Parser<Array<T>> { manyN(n: 1, inner) }
+public func many2<T>(_ inner: some Parser<T>) -> some Parser<Array<T>> { manyN(n: 2, inner) }
+
+fileprivate func manyN<T>(n: Int, _ inner: some Parser<T>) -> some Parser<Array<T>> {
+	InlineParser { input in
+		guard let zeroOut = many0(inner).parse(&input),
+			  zeroOut.count >= n
+		else { return nil }
+		return zeroOut
+	}
+}
